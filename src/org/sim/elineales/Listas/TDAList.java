4ef3b0b6348b3c66e5b01eba232dd7b5f18fd10c
DiegoSH2013 <diego.soto@ucsm.edu.pe>
2021-05-31 22:19:31 -0500

    cambio de ubicacion de listas genericas

---
+++ src/org/sim/elineales/Listas/TDAList.java
@@ -0,0 +1,12 @@
+package org.sim;
+
+public interface TDAList <T> {
+	boolean isEmptyList();
+	int length();
+	void destroyList();
+	int search(T x);
+	void insertLast(T x);
+	void insertFirst(T x);
+	void remove(T x);
+	String toString();
+}
