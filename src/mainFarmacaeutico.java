97517651dcb7106b05625b2a27f9ec8586c547d9
JackelinePeña <jackeline.pena@ucsm.edu.pe>
2021-06-25 23:30:30 -0500

    Se añadio y modifico un switchCase en el main de Farmaceutico

--- src/mainFarmacaeutico.java
+++ src/mainFarmacaeutico.java
@@ -1,23 +1,17 @@
-
 import java.util.Scanner;
-
-
 import org.sim.poo.Farmaceutico;
 import org.sim.poo.Producto;
 
 public class mainFarmacaeutico {
     public static void main(String[] args) {
 
-    	
     	Producto pro = new Producto();
-    	
     	Farmaceutico farma = new Farmaceutico();
     	farma.RegistrarMedicamento(pro);
     	farma.VerMedicamentos(pro);
     	farma.IniciarSesion();
-    	
 
-    	Farmaceutico Fabricio = new Farmaceutico();
+    	Farmaceutico farmaceuticos = new Farmaceutico();
     	
     	System.out.println("Como usuario usted podra realizar lo siguiente:");
         System.out.println("""
@@ -29,20 +23,31 @@ public class mainFarmacaeutico {
     	
     	int opcion = 0;
         System.out.print("\nA continuacion seleccione la accion que quiera realizar (1/2/3/4/5): ");
+        System.out.print("""
+        		\t1: Iniciar Sesion
+        		\t2 Registrar Medicamento
+        		\t3 Ver Medicamentos
+        		\t4 Salir""");
+        
         Scanner s = new Scanner(System.in);
 
         opcion = s.nextInt();
 
         switch (opcion) {
         case 1:
-        	Fabricio.IniciarSesion(); //Guardar datos Listas?
+        	farmaceuticos.IniciarSesion();
             break;
         case 2:
-        	Producto Veneno = new Producto(); //Llame al contructor 
-            Fabricio.RegistrarMedicamento(Veneno); 
-            break;
+        	Producto Productos = new Producto();
+        	farmaceuticos.RegistrarMedicamento(Productos); 
+            break;   
         case 3:
-        	System.out.println("Dejen de robar codigo P.D. Jackeline consigue el codigo de polar");
+        	Producto Productos2 = new Producto();
+        	farmaceuticos.VerMedicamentos(Productos2); 
+            break;
+            
+        case 4:
+        	System.out.println("Usted salio del sitio");
             break;
             
     }
