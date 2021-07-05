fb5f5c9fc55ffe8a04c485372fd39b87c15b3978
Overzpf <82428450+Overzpf@users.noreply.github.com>
2021-07-05 00:05:00 -0500

    Se modifico los tipos de datos

--- src/org/sim/poo/Persona.java
+++ src/org/sim/poo/Persona.java
@@ -1,52 +1,38 @@
 package org.sim.poo;
 import java.util.Scanner;
 
-public class Persona <T> {
+public class Persona {
 
-	private T nombre;
-	private T dni;
-	private T edad;
-	private T direccion;
-	private T FechaNacimiento;
-	private T estadoCivil;
+	private String nombre;
 	private String apellido;
+	private int dni;
+	private int edad;
+	private String direccion;
+	private String FechaNacimiento;
+	private String estadoCivil;
 	private String correo;
 
-	public Persona(T nombre, T dni) {
+	public Persona(String nombre, int dni) {
+		this.nombre=nombre;
+		this.dni=dni;
+	
+	 
+	}
+	public Persona() {
 		super();
-		this.nombre = nombre;
-		this.dni = dni;
 	}
 
-	public Persona(T nombre, T dni, T edad, T direccion, T fechaNacimiento, T estadoCivil) {
-		super();
-		this.nombre = nombre;
-		this.dni = dni;
-		this.edad = edad;
-		this.direccion = direccion;
-		setFechaNacimiento(fechaNacimiento);
-		this.estadoCivil = estadoCivil;
-	}
 
 	public Persona(String nombre, String apellido, int edad, String correo) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.correo = correo;
 	}
 
-	@Override
-	public String toString() {
-		return "Persona [nombre=" + nombre + ", dni=" + dni + ", edad=" + edad + ", direccion=" + direccion
-				+ ", FechaNacimiento=" + FechaNacimiento + ", estadoCivil=" + estadoCivil + "]";
-	}
 
-	public void nombrePersona(T nombre) {
-		Scanner entrada = new Scanner(System.in);
-		nombre = (T) entrada.nextLine();
-	}
-
-	public T getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(T nombre) {
+	public void nombrePersona(String nombre) {		
+    public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
@@ -58,54 +44,11 @@ public class Persona <T> {
 		return this.apellido;
 	}
 
-	public void dniPersona(String dniPersona) {
+	public void dniPersona(String dniPersona) {		
 		Scanner entrada = new Scanner(System.in);
-		dniPersona = entrada.nextLine();
-	}
-
-	public T getDni() {
-		return dni;
-	}
-
-	public void setDni(T dni) {
-		this.dni = dni;
-	}
-
-	public T getEdad() {
-		return edad;
-	}
-
-	public void setEdad(T edad) {
-		this.edad = edad;
-	}
-
-	public T getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(T direccion) {
-		this.direccion = direccion;
-	}
-
-	public void estadoCivil(T estadoCivil) {
-		Scanner entrada = new Scanner(System.in);
-		estadoCivil = (T) entrada.nextLine();
-	}
-
-	public T getEstadoCivil() {
-		return estadoCivil;
-	}
-
-	public void setEstadoCivil(T estadoCivil) {
+		dniPersona = entrada.nextLine();		
+    public void setEstadoCivil(String estadoCivil) {
 		this.estadoCivil = estadoCivil;
-	}
-
-	public T getFechaNacimiento() {
-		return FechaNacimiento;
-	}
-
-	public void setFechaNacimiento(T fechaNacimiento2) {
-		FechaNacimiento = fechaNacimiento2;
 	}
 
 	public void setCorreo(String correo) {
