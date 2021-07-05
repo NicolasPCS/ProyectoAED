98a9ece5293016a516f2e9d98d10019d133d800a
Overzpf <82428450+Overzpf@users.noreply.github.com>
2021-07-05 00:03:43 -0500

    Se creo el Menú de Inicio de sesión

--- src/mainFarmacaeutico.java
+++ src/mainFarmacaeutico.java
@@ -1,39 +1,84 @@
 import java.util.Scanner;
 
+import org.sim.elineales.Listas.ListLinked;
 import org.sim.poo.Farmaceutico;
 import org.sim.poo.Producto;
 
 public class mainFarmacaeutico {
+	
+	private static Farmaceutico usuarioF =new Farmaceutico();
     public static void main(String[] args) {
-    	System.out.println("Como usuario usted podra realizar lo siguiente:");
-        Farmaceutico Fabricio = new Farmaceutico("","",0,"","","");
-
-        System.out.println("Como usuario usted podra realizar lo siguiente:");
+    	ListLinked <Farmaceutico> farmaceuticos = new ListLinked();
+    	System.out.println("Selecciona una opcion:");
         System.out.println("""
-                \t1. Crear una cuenta
-                \t2. Ingresar medicamentos
-                \t3. Modificar Stock
-                \t4. Desvincular Inventario
-                \t5. Salir""");
-
+                \t1. Iniciar Sesion
+                \t2. Registrarse""");
+        
         int opcion = 0;
-        System.out.print("\nA continuacion seleccione la accion que quiera realizar (1/2/3/4/5): ");
+        System.out.print("\nA continuacion seleccione la accion que quiera realizar (1/2): ");
+        Scanner s = new Scanner(System.in);
+
+        opcion = s.nextInt();
+
+        if(opcion!=1 && opcion!=2 ) {
+        	System.out.println("Opcion no valida:");
+        }
+        
+        int usuario;
+        String contra;
+        if(opcion==1) {
+        	System.out.println("usuario:");
+        	usuario = s.nextInt();
+        	System.out.println("contraseña:");
+        	contra = s.nextLine();
+        	usuarioF=new Farmaceutico(usuario,contra);
+        	int e = farmaceuticos.search(usuarioF);
+        	if (e==-1) {
+        		System.out.println("Usuario y/o contraseña incorrectos");
+        		
+        	}
+        	else {
+        		usuarioF=farmaceuticos.getNodeAt(e).getData();
+        		System.out.println("Bienvenido "+usuarioF.getNombre());
+        		mostrarMenu();
+        		
+        	}
+        }else {
+        	System.out.println(
+        }
+
+        
+    }
+    
+    private static void mostrarMenu () {
+    	
+    	System.out.println("Como usuario usted podra realizar lo siguiente:");
+        System.out.println("""
+                \t1. Ingresar medicamentos
+                \t2. Modificar Stock
+                \t3. Desvincular Inventario
+                \t4. Salir""");
+    
+        int opcion = 0;
+        System.out.print("\nA continuacion seleccione la accion que quiera realizar (1/2/3/4): ");
         Scanner s = new Scanner(System.in);
 
         opcion = s.nextInt();
 
         switch (opcion) {
             case 1:
-                Fabricio.IniciarSesion(); //Guardar datos Listas?
+            	
+                usuarioF.RegistrarMedicamento(null);
                 break;
             case 2:
-                Producto Veneno = new Producto(); //Llame al contructor
-                Fabricio.RegistrarMedicamento(Veneno);
+                Producto Paracetamol = new Producto(); //Llame al contructor
+                Fabricio.RegistrarMedicamento(Paracetamol);
                 break;
             case 3:
-                System.out.println("Dejen de robar codigo P.D. Jackeline consigue el codigo de polar");
+                System.out.println("");
                 break;
 
         }
+    	
     }
 }
