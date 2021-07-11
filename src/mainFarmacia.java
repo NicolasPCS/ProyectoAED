ba4db2f4f5de63d62bfef147f47081a8f7df25bc
FABRICIO PILCO <82428450+Fabricio-Pilco@users.noreply.github.com>
2021-07-11 11:24:11 -0500

    Se añadió el acceso al sistema de farmacias

--- src/mainFarmacia.java
+++ src/mainFarmacia.java
@@ -1,9 +1,26 @@
-
+import java.util.Scanner;
+import org.sim.elineales.Listas.ListLinked;
+import org.sim.poo.Farmaceutico;
+import org.sim.poo.Farmacia;
+import org.sim.poo.Producto;
 public class mainFarmacia {
-
+	@SuppressWarnings({ "null", "resource" })
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ingresar = new Scanner(System.in);
+		String opcion;
+		int opcion1;
+		int opcion2=0;
+		int posicion;
+		ListLinked<Farmacia> sistema = new ListLinked<Farmacia>();
+		System.out.println("Desea cerrar el sistema? (Si/No)");
+		opcion = ingresar.nextLine();
+		while(!opcion.equalsIgnoreCase("Si")) {
+			System.out.println("<=======\tBienvenido al Sistema de la Farmacia\t=======>\n Por favor ingrese una opcion:\n"
+					+ "\t1. Ingresar a una Farmacia\n\t2. Agregar una Farmacia\n\t3. Cerrar el sistema");
+			opcion1 = ingresar.nextInt();
+			while(opcion1 <1 || opcion1 >3){
+				System.out.println("Error en la opcion ingresada, por favor vuelva a ingresar la opcion");
+				opcion1 = ingresar.nextInt();
+			}
+	
 
-	}
-
-}
