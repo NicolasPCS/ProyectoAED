3bac66810808ad5fc73c206a371c4c1a18ac1fdc
JackelinePeña <jackeline.pena@ucsm.edu.pe>
2021-07-12 05:44:33 -0500

    Se modifico la clase farmaceutico para el almacenamiento de ingreso de farmaceutico en el mainFarmaceutico. Se aladio ListLinked para el almacenamiento de los datos. Se modifico el main para una mayor funcionalidad del clase implementado el ingreso de farmaceutico con su usuario y contraseña.

--- src/org/sim/poo/Farmacia.java
+++ src/org/sim/poo/Farmacia.java
@@ -1,65 +1,176 @@
 package org.sim.poo;
-
 import java.util.Date;
 import java.util.Scanner;
 
-public class Farmacia {
+import org.sim.elineales.Listas.ListLinked;
+import org.sim.hashcode.HashC;
+import java.math.*;
+public class Farmacia implements Comparable<Farmacia>{
     private int idFarmacia = 0;
     private String dirSede;
     private boolean disponible;
-    private Date horarioAtencion;
+    private String horarioAtencion;
     private String telefono;
 	private String producto;
-	private Producto producto1;
-	
-	
-	public Farmacia() {}
+	private static HashC<Producto> producto2;
+	private ListLinked<Producto> producto1;
+	private ListLinked<Farmaceutico> farmaceuticos = new ListLinked<Farmaceutico>();
+	public int getIdFarmacia() {
+		return idFarmacia;
+	}
 
-	public Farmacia(String dirSede, Date horarioAtencion, String telefono) {
-        this.idFarmacia += 1;
-        this.dirSede = dirSede;
-        this.disponible = false;
-        this.horarioAtencion = horarioAtencion;
-        this.telefono = telefono;
-    }
+	public HashC<Producto> getProducto2() {
+		return producto2;
+	}
 
-	public void agregarFarmaceuticos() {
-		Scanner enter = new Scanner (System.in);
-		System.out.println("INGRESE CUANTOS FARMACEUTICOS VA A REGISTRAR");
-		int num = enter.nextInt();
-		enter.nextLine();
-		
-		for (int i = 0; i < num; i++) {
-			System.out.println("-------------------------------------------");
-			System.out.print("Nombre: ");
-			String nombre = enter.nextLine();
-			
-			System.out.print("Apellido: ");
-			String apellido  = enter.nextLine();
-			
-			System.out.print("Edad: ");
-			int edad = enter.nextInt();
-			enter.nextLine();
-			
-			System.out.print("Correo: ");
-			String correo = enter.nextLine();
-			
-			System.out.print("Farmacia : ");
-			String farmacia = enter.nextLine();
-			
-			System.out.print("Contraseña: ");
-			String contra = enter.nextLine();
-			
+	public void setProducto2(HashC<Producto> producto2) {
+		this.producto2 = producto2;
+	}
 
-			Farmaceutico f = new Farmaceutico(nombre, apellido, edad, correo, farmacia, contra);
-			System.out.println("Farmaceutico registrado:\n" + f.getNombre() + 
-					" - " + f.getApellido() + " - " + f.getEdad() + " - " + f.getCorreo() + 
-					" - " + f.getFarmacia());
-		}
-		
+	public void setIdFarmacia(int idFarmacia) {
+		this.idFarmacia = idFarmacia;
+	}
+
+	public String getDirSede() {
+		return dirSede;
+	}
+
+	public void setDirSede(String dirSede) {
+		this.dirSede = dirSede;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public String getHorarioAtencion() {
+		return horarioAtencion;
+	}
+
+	public void setHorarioAtencion(String horarioAtencion) {
+		this.horarioAtencion = horarioAtencion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getProducto() {
+		return producto;
+	}
+
+	public void setProducto(String producto) {
+		this.producto = producto;
 	}
 	
-    public void verHorario() {
+	public ListLinked<Producto> getProducto1() {
+		return producto1;
+	}
+
+	public void setProducto1(ListLinked<Producto> producto1) {
+		this.producto1 = producto1;
+	}
+
+	public Farmacia() {
+	}
+	public Farmacia(int idFarmacia) {
+		this.idFarmacia = idFarmacia;
+	}
+	public Farmacia(int idFarmacia,String dirSede, String horarioatencion, String telefono) {
+        this.idFarmacia = idFarmacia; 
+        this.dirSede = dirSede;
+        this.disponible = false;
+        this.horarioAtencion = horarioatencion;
+        this.telefono = telefono;
+    }
+	@SuppressWarnings({ "resource", "unused" })
+	public void agregarFarmaceutico() {
+		String opcion;
+		Scanner enter = new Scanner (System.in);
+		opcion = "Si";
+		while (!opcion.equalsIgnoreCase("No")) {
+		if(this.farmaceuticos ==null) {
+			System.out.println("<=======\tNuevo Farmaceutico\t=======>");
+			System.out.println("Codigo: ");
+			String codigo = enter.next();
+			System.out.println("Nombre: ");
+			String nombre = enter.next();
+			System.out.println("Apellido: ");
+			String apellido  = enter.next();
+				
+			System.out.println("Edad: ");
+			int edad = enter.nextInt();
+			enter.nextLine();
+				
+			System.out.println("Correo: ");
+			String correo = enter.next();
+				
+			/*System.out.print("Farmacia : ");
+			String farmacia = enter.nextLine();*/
+				
+			System.out.println("Contraseña: ");
+			String contra = enter.next();
+			System.out.println("Farmaceutico registrado:\n" + nombre + 
+						" - " + apellido + " - " + edad + " - " + correo);
+			Farmaceutico f = new Farmaceutico(codigo,nombre, apellido, edad, correo,Farmacia.this,contra);
+			this.farmaceuticos.insertFirst(f);
+			
+		}
+		else {
+			System.out.println("<=======\tNuevo Farmaceutico\t=======>");
+			System.out.println("\tCodigo: ");
+			String codigo = enter.next();
+			System.out.println("\tNombre: ");
+			String nombre = enter.next();
+				
+			System.out.println("\tApellido: ");
+			String apellido  = enter.next();
+				
+			System.out.println("\tEdad: ");
+			int edad = enter.nextInt();
+				
+			System.out.println("\tCorreo: ");
+			String correo = enter.next();
+				
+			/*System.out.print("Farmacia : ");
+			String farmacia = enter.nextLine();*/
+				
+			System.out.println("\tContraseña: ");
+			String contra = enter.next();
+			System.out.println("Farmaceutico registrado:\n" + codigo+ " - " + nombre + 
+						" - " + apellido + " - " + edad + " - " + correo);
+			Farmaceutico f = new Farmaceutico(nombre, apellido, edad, correo,Farmacia.this,contra);
+			this.farmaceuticos.insertLast(f);
+		}
+		System.out.println("Desea seguir con el registro de farmaceuticos (Si/No)");
+		opcion = enter.next();
+		}
+		System.out.println("Saliendo del ingreso de farmaceuticos");
+	}
+	public static void verReporteProductos(ListLinked<Producto> pro) {
+		producto2 = new HashC<Producto>(pro.length());
+		int total = pro.length();
+		for(int i = 0;i<total;i++) {
+			char [] c = pro.getNodeAt(i).getData().getCodigo().toCharArray();
+			int sum = 0;
+			for(int i1 = 0;i<c.length;i++) {
+				sum+= c[i1]*Math.pow(10, i1);
+			}
+			producto2.insertarEncadenamiento(sum, pro.getNodeAt(0).getData());
+			
+		}
+		System.out.println(producto2);
+	}
+    @SuppressWarnings("resource")
+	public void verHorario() {
     	
     	int opc = 0;
     	
@@ -115,15 +226,30 @@ public class Farmacia {
 
     	Scanner entrada11 = new Scanner(System.in);
 		Producto = entrada11.nextLine();
-
 		entrada = new Scanner(System.in);
 
-
-
-		
     	
     }
-
+    public static Farmacia ingresarDatosFarm() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("<=======\tNUEVA FARMACIA\t=======>");
+		System.out.println("\tId de la Farmacia");
+		int IdFarmacia = s.nextInt();
+		System.out.println("\tNombre de la sede: ");
+		String nombre = s.next();
+		System.out.println("\tHorario de atencion: ");
+		String horarioatencion = s.next();
+		System.out.println("\tTelefono: ");
+		String telefono = s.next();
+		return new Farmacia(IdFarmacia,nombre,horarioatencion,telefono);
+    }
+    public static Farmacia BusquedaDatosFarm() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("<=======\tFARMACIA EXISTENTE\t=======>");
+		System.out.println("\tId de la Farmacia");
+		int IdFarmacia = s.nextInt();
+		return new Farmacia(IdFarmacia);
+    }
     @Override
     public String toString() {
         return "Farmacia{" +
@@ -134,4 +260,39 @@ public class Farmacia {
                 ", telefono='" + telefono + '\'' +
                 '}';
     }
+	public boolean equals(Object o) {
+		if(o instanceof Farmacia) {
+			Farmacia a = (Farmacia) o;
+			return this.idFarmacia==a.idFarmacia;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean equals(Farmacia o) {
+		return this.idFarmacia == o.idFarmacia;
+	}
+	@Override
+	public int compareTo(Farmacia o) {
+		// TODO Auto-generated method stub
+		if(o instanceof Farmacia) {
+			Farmacia a = (Farmacia) o;
+			if(this.idFarmacia>a.idFarmacia) return 1;
+			if(this.idFarmacia<a.idFarmacia) return -1;
+			else {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+
+	public ListLinked<Farmaceutico> getFarmaceuticos() {
+		return farmaceuticos;
+	}
+
+	public void setFarmaceuticos(ListLinked<Farmaceutico> farmaceuticos) {
+		this.farmaceuticos = farmaceuticos;
+	}
 }
