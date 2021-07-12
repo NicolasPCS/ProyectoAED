28eab8bff96649245a41f69d3e1a5589ebd11ba7
JackelinePeña <jackeline.pena@ucsm.edu.pe>
2021-07-12 06:14:31 -0500

    Se añadio MainFarmacia para implementar su busqueda con el ComparaTo y el ingreso de datos

---
+++ src/mainFarmacia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import org.sim.elineales.Listas.ListLinked;
+import org.sim.poo.Farmaceutico;
+import org.sim.poo.Farmacia;
+import org.sim.poo.Producto;
+public class mainFarmacia {
+	@SuppressWarnings({ "null", "resource" })
+	public static void main(String[] args) {
+		Scanner ingresar = new Scanner(System.in);
+		String opcion;
+		int opcion1;
+		int opcion2=0;
+		int posicion;
+		ListLinked<Farmacia> sistema = new ListLinked<Farmacia>();
+		System.out.println("Desea cerrar el sistema? (Si/No)");
+		
+			
+		
+	}
+
+}
+
