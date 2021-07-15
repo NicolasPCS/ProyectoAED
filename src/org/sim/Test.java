35e122877cc6b967d7be0e7c7fcf53b1dcdb27ee
FABRICIO PILCO <82428450+Fabricio-Pilco@users.noreply.github.com>
2021-07-14 23:55:14 -0500

    Se modifico la clase "Test"

--- src/org/sim/Test.java
+++ src/org/sim/Test.java
@@ -1,20 +1,33 @@
 package org.sim;
 
+import java.util.Scanner;
+
+import org.sim.elineales.Listas.ListLinked;
+import org.sim.elineales.Stacks.ExceptionIsEmpty;
 import org.sim.poo.*;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        /*LinkedStack<Integer> b = new LinkedStack<Integer>();
-        
-        b.push(7);
-        b.toString();
-        */
-        Farmacia farmacia = new Farmacia();
-        farmacia.agregarFarmaceuticos();
-        // LinkedStack<Integer> b = new LinkedStack<Integer>();
-        
-        // b.push(7);
-        // b.toString();
+    public static void main(String[] args) throws ExceptionIsEmpty {
+        Scanner s = new Scanner(System.in);
+    	String opcion;
+        int opcion1;
+        System.out.println("¿Desea abrir el sistema?(Si/No)");
+        opcion = s.next();
+        while(!opcion.equalsIgnoreCase("No")) {
+	        System.out.println("<=======BIENVENIDO AL GRAN SISTEMA DE FARMACIAS=======>");
+	        System.out.println("Por favor Elija el sistema al cual desea acceder:");
+	        System.out.println("\t1. Clientes\n\t2. Farmaceuticos\n\t3. Farmacias\n\t4. Salir del Sistema");
+	        opcion1 = s.nextInt();
+	        switch(opcion1) {
+	        case 1:
+	        	Clientes();
+	        case 2:
+	        	Farmaceuticos();
+	        case 3:
+	        	Farmacias();
+	        case 4:
+	        	System.out.println("Saliendo del sistema, Adios!...");
+	        	opcion = "No";
+	        }
+        }
     }
-}
