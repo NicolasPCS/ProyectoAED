7db73040820f110222387b663bde8661a5db28da
FABRICIO PILCO <82428450+Fabricio-Pilco@users.noreply.github.com>
2021-07-14 23:52:26 -0500

    Se modifico la clase Stock

--- src/org/sim/poo/Stock.java
+++ src/org/sim/poo/Stock.java
@@ -1,17 +1,34 @@
 package org.sim.poo;
 
-public class Stock {
-    private int idFarmacia;
-    private int tipoStock;
-    private Producto arrProductos;
+import java.util.LinkedList;
 
-    public Stock(int idFarmacia, int tipoStock) {
-        this.idFarmacia = idFarmacia;
+public class Stock {
+    private Farmacia farmacia;
+    private int tipoStock;
+    private Producto producto = new Producto();
+    public int getTipoStock() {
+		return tipoStock;
+	}
+
+	public void setTipoStock(int tipoStock) {
+		this.tipoStock = tipoStock;
+	}
+
+	public Stock(Farmacia farmacia, int tipoStock) {
+        this.setFarmacia(farmacia);
         this.tipoStock = tipoStock;
-        this.arrProductos = new Producto();
     }
 
     public void anadirProd() {
-
+    	return;
     }
-}
+    public String ObtenerTipoStock(){
+    	if(this.tipoStock == 1) {
+    		return "Medicamento";
+    	}
+    	else if(this.tipoStock == 2) {
+    		return "Producto";
+    	}
+    	else
+    		return "Este tipo de stock no existe";
+    }
