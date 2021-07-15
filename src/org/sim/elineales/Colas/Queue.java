ef00dfbb57e8f6f88522ee1db418557297a4006a
Nicolás Caytuiro Silva <53417263+NicolasPCS@users.noreply.github.com>
2021-07-14 23:48:24 -0500

    Se modifico la estructura del proyecto

--- src/org/sim/elineales/Colas/Queue.java
+++
@@ -1,9 +0,0 @@
-package org.sim.elineales.Colas;
-
-public interface Queue<E> {
-    void enqueue(E x);
-    E dequeue() throws ExceptionIsEmpty;
-    E front() throws ExceptionIsEmpty;
-    E back() throws ExceptionIsEmpty;
-    boolean isEmpty();
-}
--- src/org/sim/elineales/Stacks/Stack.java
+++
@@ -1,8 +0,0 @@
-package org.sim.elineales.Stacks;
-
-public interface Stack <E> {
-	void push(E x);
-	E pop() throws ExceptionIsEmpty;
-	E top() throws ExceptionIsEmpty;
-	boolean isEmpty();
-}
